dd356bd52fd508e217a23fa6c318ea26e6dc387f
gulkayir1 <de.gulkayir@gmail.com>
2021-11-28 18:02:33 +0600

    staticfiles dirs

--- twitter_api/settings.py
+++ twitter_api/settings.py
@@ -126,6 +126,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIRS = (
+os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Default primary key field type
